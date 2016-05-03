125063a82dcb8e0d7a6e224ffff95523c83b11e5
BrunaKimura <brunamk@al.insper.edu.br>
2016-05-03 19:56:35 -0300

    Criei as init's para os sons, porém sem os sons

--- imagem_som.py
+++ imagem_som.py
@@ -1,13 +1,23 @@
 from tkinter import PhotoImage
+import winsound as ws
 
 class Imagens_sons:
 
     def __init__(self):
-        self.cachorro = PhotoImage(file=".gif")
-        self.gato = PhotoImage(file=".gif")
-        self.arara = PhotoImage(file=".gif")
-        self.vaca = PhotoImage(file=".gif")
-        self.hamster = PhotoImage(file=".gif")
-        self.pato = PhotoImage(file=".gif")
-        self.cavalo = PhotoImage(file=".gif")
-        self.porco = PhotoImage(file=".gif")
+        self.Icachorro = PhotoImage(file=".gif")
+        self.Igato = PhotoImage(file=".gif")
+        self.Iarara = PhotoImage(file=".gif")
+        self.Ivaca = PhotoImage(file=".gif")
+        self.Ihamster = PhotoImage(file=".gif")
+        self.Ipato = PhotoImage(file=".gif")
+        self.Icavalo = PhotoImage(file=".gif")
+        self.Iporco = PhotoImage(file=".gif")
+        
+        self.Scachorro = ws.PlaySound('latido.wav', ws.SND_FILENAME)
+        self.Sgato =  ws.PlaySound('', ws.SND_FILENAME)
+        self.Sarara =  ws.PlaySound('', ws.SND_FILENAME)
+        self.Svaca =  ws.PlaySound('', ws.SND_FILENAME)
+        self.Shamster =  ws.PlaySound('', ws.SND_FILENAME)
+        self.Spato =  ws.PlaySound('', ws.SND_FILENAME)
+        self.Scavalo =  ws.PlaySound('', ws.SND_FILENAME)
+        self.Sporco =  ws.PlaySound('', ws.SND_FILENAME)
