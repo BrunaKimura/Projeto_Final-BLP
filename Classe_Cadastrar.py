16d1615f96db45b0656ebbea2df267762b36cac0
patrick23232 <patrick.camara.ismart@gmail.com>
2016-05-04 19:44:18 -0300

    Criação da classe cadastrar e modificação em algumas funções

---
+++ Classe_Cadastrar.py
@@ -0,0 +1,9 @@
+class cadastrar():
+    
+    def __init__(self):
+        self.cadastros=dict
+        
+    def adicionar_cadastro(self, login, senha, sexo, idade):
+        if not login in self.cadastros:        
+            self.cadastros[login]=[senha, sexo, idade]
+        
