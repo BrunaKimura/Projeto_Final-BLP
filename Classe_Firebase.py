999246d6b786bd5018512f8b4147933323c85f82
BrunaKimura <brunamk@al.insper.edu.br>
2016-05-09 17:22:47 -0300

    Adicionei alguns comentarios na clase firebase

--- Classe_Firebase.py
+++ Classe_Firebase.py
@@ -1,9 +1,12 @@
 import firecall
 
 class Firebase:
-    
+
     def __init__(self):
+        #chamar o endereço do firebase        
         self.memoria_firecall = firecall.Firebase("https://estatistica-animal.firebaseio.com/")
         
+    #Criar local de armazenamento de dados
     def Salvar_cadastro(self, Dicionario):
         self.memoria_firecall.put_sync(point="/Dados_usuario", data=Dicionario)
+        
