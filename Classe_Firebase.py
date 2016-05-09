13af85837cce4c9f275523ca1cf8470fe5828daa
BrunaKimura <brunamk@al.insper.edu.br>
2016-05-09 17:15:39 -0300

    Criei uma função que armazena os dados do usuarios na nuvem

--- Classe_Firebase.py
+++ Classe_Firebase.py
@@ -4,3 +4,6 @@ class Firebase:
     
     def __init__(self):
         self.memoria_firecall = firecall.Firebase("https://estatistica-animal.firebaseio.com/")
+        
+    def Salvar_cadastro(self, Dicionario):
+        self.memoria_firecall.put_sync(point="/Dados_usuario", data=Dicionario)
