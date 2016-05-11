e09502956d2ea6e38dad025d807c59f9cdfad6b9
patrick23232 <patrick.camara.ismart@gmail.com>
2016-05-11 17:50:41 -0300

    A

--- Classe_Jogar.py
+++ Classe_Jogar.py
@@ -40,9 +40,7 @@ class Jogo():
             return "Porco"
             
     def verifica_jogada(self, Lista_Jogada):
-        if len(self.Lista_Jogada)==0:
-            return 0
-        elif len(self.Lista_Jogada)==2: 
+        if len(self.Lista_Jogada)==2: 
             if self.Lista_Jogada[0]==self.Lista_Jogada[1]: 
                 for i in range(4):
                     for e in range(4):
@@ -65,5 +63,6 @@ class Jogo():
     def limpa_jogada(self):
         self.Lista_Jogada=[]
         self.Lista_Botão=[]
+
 
 
