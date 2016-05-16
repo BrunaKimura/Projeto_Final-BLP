d137249457a41fb85a40d188b6307d72e6ff909e
BrunaKimura <brunamk@al.insper.edu.br>
2016-05-16 16:46:42 -0300

    arrumei comentario da classe firebase

--- Classe_Firebase.py
+++ Classe_Firebase.py
@@ -12,7 +12,7 @@ class Firebase:
         self.memoria_firecall.put_sync(point="/Dados_usuario/{0}/sexo".format(login), data=sexo)
         self.memoria_firecall.put_sync(point="/Dados_usuario/{0}/idade".format(login), data=idade)
         
-    #Retonar o jogador para teste
+    #Checar se o usuário já existe e se sim se sua senha está correta
     def Checar_jogador(self, login, senha):
         C = eval(self.memoria_firecall.get_sync(point="/Dados_usuario"))
         if login in C:
