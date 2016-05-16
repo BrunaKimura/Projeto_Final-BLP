77200feaf9f7b6dea05489b07aaa0845972c3b62
BrunaKimura <brunamk@al.insper.edu.br>
2016-05-16 16:00:58 -0300

    arrumei a def que salva o jogador

--- Classe_Firebase.py
+++ Classe_Firebase.py
@@ -7,14 +7,17 @@ class Firebase:
         self.memoria_firecall = firecall.Firebase("https://estatistica-animal.firebaseio.com/")
         
     #Criar local de armazenamento de dados
-    def Salvar_cadastro(self, Dicionario):
-        self.memoria_firecall.put_sync(point="/Dados_usuario", data=Dicionario)
+    def Salvar_cadastro(self, login, senha, sexo, idade):
+        self.memoria_firecall.put_sync(point="/Dados_usuario/{0}/senha".format(login), data=senha)
+        self.memoria_firecall.put_sync(point="/Dados_usuario/{0}/sexo".format(login), data=sexo)
+        self.memoria_firecall.put_sync(point="/Dados_usuario/{0}/idade".format(login), data=idade)
         
     #Retonar o jogador para teste
     def Checar_jogador(self, login):
         C = eval(self.memoria_firecall.get_sync(point="/Dados_usuario"))
         if login in C:
-            return 1
+            D = eval(self.memoria_firecall.get_sync(point="/Dados_usuario/{0}".format(login)"))
+            if senha in D
         else:
             return -1
         
