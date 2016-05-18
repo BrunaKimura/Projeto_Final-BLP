7fce8048b00d90654ed888538bf0bdd3651bbef4
patrick23232 <patrick.camara.ismart@gmail.com>
2016-05-18 17:53:26 -0300

    Criação da instância jogador na classe_jogar e junção da classe comprar com o firebase

--- Classe_Logar.py
+++ Classe_Logar.py
@@ -1,12 +1,15 @@
 from Classe_Firebase import Firebase
+from Classe_Jogar import Jogo
 
 class Login():
     
     def __init__(self):
         self.firebase = Firebase()
+        self.jogo=Jogo()
     
     def verifica(self, login, senha):
         if self.firebase.Checar_jogador(login, senha)==1:
+            self.jogo.jogador=login
             return 1
         elif self.firebase.Checar_jogador(login, senha)==-1:
             return 0
