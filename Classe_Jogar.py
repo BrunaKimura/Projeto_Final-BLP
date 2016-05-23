2a573122d417b2784f7c6f8103d5c8a0915fc960
lemelusca <l.leme.silva@hotmail.com>
2016-05-23 17:36:49 -0300

    Ajueste dnv

--- Classe_Jogar.py
+++ Classe_Jogar.py
@@ -19,7 +19,7 @@ class Jogo():
             self.Lista_Dobrada.append(e)
         self.Lista_Embaralhada=np.random.permutation(self.Lista_Dobrada)
         self.Tabuleiro=[[self.Lista_Embaralhada[0], self.Lista_Embaralhada[1], self.Lista_Embaralhada[2], self.Lista_Embaralhada[3]], [self.Lista_Embaralhada[4], self.Lista_Embaralhada[5], self.Lista_Embaralhada[6], self.Lista_Embaralhada[7]], [self.Lista_Embaralhada[8], self.Lista_Embaralhada[9], self.Lista_Embaralhada[10], self.Lista_Embaralhada[11]], [self.Lista_Embaralhada[12], self.Lista_Embaralhada[13], self.Lista_Embaralhada[14], self.Lista_Embaralhada[15]]]
-        print(self.jogador, self.moeda)
+
         
     def recebe_jogada(self, linha, coluna):
         self.Lista_Jogada.append(self.Tabuleiro[linha][coluna])
