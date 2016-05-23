d7887f2b9e87ad78ed3609764be8e34b14cece83
patrick23232 <patrick.camara.ismart@gmail.com>
2016-05-23 17:11:32 -0300

    Correção na função verificar_login do MENU

--- Classe_Cadastrar.py
+++ Classe_Cadastrar.py
@@ -7,6 +7,10 @@ class cadastrar():
         
     def adicionar_cadastro(self, login, senha, sexo, idade):
         if self.firebase.Checar_jogador(login, senha)== -1:  
+            if sexo==("Masculino" or "masculino" or "MASCULINO" or "m" or "M"):
+                sexo="M"
+            elif sexo==("Feminino" or "feminino" or "MASCULINO" or "f" or "F"):
+                sexo="F"
             self.firebase.Salvar_cadastro(login, senha, sexo, idade)
             return 1
         elif self.firebase.Checar_jogador(login,senha)== (1 or 0):
